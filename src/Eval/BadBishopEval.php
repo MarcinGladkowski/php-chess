b30e1d66032059db35cd2817207513286f626693
MarcinGladkowski <marcingladkowski@gmail.com>
2022-07-19 07:58:15 +0200

    bad_bishop: checking the closest fields

--- src/Eval/BadBishopEval.php
+++ src/Eval/BadBishopEval.php
@@ -6,6 +6,8 @@ namespace Chess\Eval;
 
 use Chess\Board;
 use Chess\PGN\AN\Color;
+use Chess\PGN\AN\Piece;
+use Chess\Piece\AbstractPiece;
 
 class BadBishopEval extends AbstractForkEval
 {
@@ -23,10 +25,27 @@ class BadBishopEval extends AbstractForkEval
 
     public function eval(): array
     {
-        $this->result = [
-            Color::W => 0,
-            Color::B => 1,
-        ];
+        /** @var AbstractPiece $piece */
+        foreach ($this->board->getPieces() as $piece) {
+            $color = $piece->getColor();
+            if ($piece->getId() === Piece::B) {
+              $mobility = $piece->getMobility();
+
+              $closestPossibleMobility = array_reduce((array) $mobility, static function($carry, $key) {
+                  if (array_key_exists(0, $key)) {
+                      $carry[] = $key[0];
+                  }
+                  return $carry;
+              }, []);
+
+              $pawns = array_map(fn($field) => $this->board->getPieceBySq($field), $closestPossibleMobility);
+              $ownPawns = array_filter($pawns, fn($pawn) => $pawn?->getColor() === $color);
+
+              if ($ownPawns >= 3) {
+                  $this->result[$color] += 1;
+              }
+            }
+        }
 
         return $this->result;
     }
